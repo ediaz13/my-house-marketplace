fb67bf64f639270123fe45a7c97198e81e96277a
ediaz13 <diazemanuel27@gmail.com>
2024-12-22 19:56:10 -0300

    82. Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+
+
+
 function App() {
   return (
     <>
-      <h1>My React App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-out" element={<SignOut />} />
+        </Routes
+      </Router>
     </>
   );
 }
