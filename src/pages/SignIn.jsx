3409ea55c3c5426e504e74906e020a5272ca1d5e
ediaz13 <diazemanuel27@gmail.com>
2024-12-24 01:37:25 -0300

    84. Sign In & Sign Up Forms

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 function SignIn() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+  const { email, password } = formData
+
+  const navigate = useNavigate()
+  
   return (
-    <div><h1>SignIn</h1></div>
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+      </div>
+      
+    </>
   )
 }
 
