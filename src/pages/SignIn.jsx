1e473c04cdf61ffad10a47f1e54b14931635bb90
ediaz13 <diazemanuel27@gmail.com>
2025-01-03 09:41:05 -0300

    87. User Sign In

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -18,9 +19,27 @@ function SignIn() {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
-    }))
+    }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -29,7 +48,7 @@ function SignIn() {
           <p className="pageHeader">Welcome Back!</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="email"
             className="emailInput"
@@ -52,8 +71,7 @@ function SignIn() {
               src={visibilityIcon}
               alt="show password"
               className="showPassword"
-              onClick={() => setShowPassword((prevState) => 
-                !prevState)}
+              onClick={() => setShowPassword((prevState) => !prevState)}
             />
           </div>
 
@@ -62,11 +80,9 @@ function SignIn() {
           </Link>
 
           <div className="signInBar">
-            <p className="signInText">
-              Sign In
-            </p>
+            <p className="signInText">Sign In</p>
             <button className="signInButton">
-              <ArrowRightIcon fill='#ffffff' width='34px' height='34px'/>
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
             </button>
           </div>
         </form>
@@ -76,7 +92,6 @@ function SignIn() {
         <Link to="/sign-up" className="registerLink">
           Sign Up Instead
         </Link>
-
       </div>
     </>
   );
