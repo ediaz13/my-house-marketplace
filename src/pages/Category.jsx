7f8ee1adb202a82d5403b72ee1c6da3708b1a8dd
ediaz13 <diazemanuel27@gmail.com>
2025-01-12 16:45:01 -0300

    95. Fetch Listings From Firebase

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
 
 function Category() {
-  return (
-    <div>Category</div>
-  )
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference
+        const listingsRef = collection(db, "listings");
+
+        // Create Query
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timeS", "desc"),
+          limit(10)
+        )
+
+        // Execute Query
+        const querySnap = await getDocs(q);
+
+        let listings = [];
+
+        querySnap.forEach((doc) => {
+          listings.push({ ...doc.data(), id: doc.id });
+        })
+
+      } catch (error) {
+        toast.error("Error loading listings");
+      }
+    };
+    fetchListings();
+  });
+
+  return <div>Category</div>;
 }
 
-export default Category
+export default Category;
