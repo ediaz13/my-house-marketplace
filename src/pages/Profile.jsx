39068525c29fcb665d2cd440dd1ff95f934bf44b
ediaz13 <diazemanuel27@gmail.com>
2025-01-14 18:49:11 -0300

    98. Start Create Listing Page.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import { useState } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
 import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
+import homeIcon from "../assets/svg/homeIcon.svg";
+
 
 function Profile() {
   const auth = getAuth();
@@ -96,6 +100,13 @@ function Profile() {
             />
           </form>
         </div>
+
+        <Link to='/create-listing' className="createListing">
+          <img src={homeIcon} alt="home" />
+          <p>Sell or Rent your Home</p>
+          <img src={arrowRight} alt="arrow right" />
+        </Link>
+
       </main>
     </div>
   );
