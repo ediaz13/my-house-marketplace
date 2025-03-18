35340da5eceecb1e39bb0ad4ae8242fe2a045d05
ediaz13 <diazemanuel27@gmail.com>
2025-03-17 21:06:46 -0300

    102. Save Listings To Firestore.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -28,7 +28,7 @@ function Explore() {
               alt='sell'
               className='exploreCategoryImg'
             />
-            <p className="exploreCategoryName">Places for sell</p>
+            <p className="exploreCategoryName">Places for sale</p>
           </Link>
         </div>
       </main>
